8caee76173778d498fe898a7818b65f4eac01e49
Dabsia <dabojohnson98@gmail.com>
2021-09-24 18:07:54 +0100

    Splash screen

---
+++ main.js
@@ -0,0 +1,16 @@
+setTimeout(ShowSplash, 3000)
+
+function ShowSplash(){
+    let splash = document.querySelector('.splash__screen');
+    let Get__started = document.querySelector('.started');
+    splash.style.display = 'none'
+    Get__started.style.display = 'block'
+}
+
+let CTA = document.getElementById('CTA')
+CTA.addEventListener('click', Redirect)
+function Redirect() {
+    
+    window.location.href =
+     window.location.href.replace('index.html', 'signup.html');
+}
